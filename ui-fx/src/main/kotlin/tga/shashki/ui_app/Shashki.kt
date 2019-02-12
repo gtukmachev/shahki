38dddea7e887d75826e80863ee714a4a7b01ae27
Grigory <grigory@clearscale.net>
2019-02-12 11:15:10 +0700

    initial implementation of FX ui

--- ui-fx/src/main/kotlin/tga/shashki/ui_app/Shashki.kt
+++ ui-fx/src/main/kotlin/tga/shashki/ui_app/Shashki.kt
@@ -1,22 +1,53 @@
 package tga.shashki.ui_app
 
 import javafx.application.Application
-import javafx.fxml.FXMLLoader.load
-import javafx.scene.Parent
+import javafx.geometry.Insets
 import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.ScrollPane
 import javafx.stage.Stage
+import javafx.scene.layout.GridPane
+
+
 /**
  * Created by grigory@clearscale.net on 2/11/2019.
  */
 class ShashkiApp : Application() {
 
-    val layout = "/resources/ShashkiApp.fxml"
+    val BUTTON_PADDING = 0.0
 
-    override fun start(primaryStage: Stage?) {
+    val NUM_BUTTON_LINES = 8
+    val BUTTONS_PER_LINE = 8
 
-        System.setProperty("prism.lcdtext", "false") // for beautiful fonts on linux
-        primaryStage?.scene = Scene(load<Parent?>(ShashkiApp::class.java.getResource(layout)))
-        primaryStage?.show()
+    override fun start(primaryStage: Stage) {
+
+
+        val grid = GridPane()
+        grid.padding = Insets(BUTTON_PADDING)
+        grid.hgap = BUTTON_PADDING
+        grid.vgap = BUTTON_PADDING
+
+        for (r in 0 until NUM_BUTTON_LINES) {
+            for (c in 0 until BUTTONS_PER_LINE) {
+                val number = r + c
+                val button = Button(number.toString()).apply {
+                    styleClass.add("field")
+                    styleClass.add( if (number % 2 == 0) "white" else "black" )
+                    setOnAction {
+                        this.styleClass.add("active")
+                    }
+                }
+
+                grid.add(button, c, (BUTTONS_PER_LINE - r)-1)
+            }
+        }
+
+        val scrollPane = ScrollPane(grid)
+
+        primaryStage.scene = Scene(scrollPane).apply {
+            stylesheets.add("main.css")
+        }
+        primaryStage.show()
     }
 
     companion object {
